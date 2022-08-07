c878ff7e366ae608c3abf243ba5a2c4ab199f305
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-07 22:33:06 +0200

    created the component for all the future text content.

---
+++ pages/Text.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+export const projects = [
+  {
+    image: '',
+    name: 'Tunes & Friends',
+    discription:
+      'This is my final project on the Technigo Bootcamp. I have had this idea for a long time, and i was finally ready to build it! Sign and mark the tunes you know or want to learn. Find your friends amongst the members and se what tunes you have in common. You can also se the sheet music for all the tunes. The music data comes from the great database The session.',
+    tags: ['react', 'hej', 'hej'],
+    deployed: 'https://www.pickuplimes.com/',
+    github: 'https://github.com/HedvigM',
+  },
+  {
+    name: 'Hej 2',
+    discription:
+      'This is my final project on the Technigo Bootcamp. I have had this idea for a long time, and i was finally ready to build it! Sign and mark the tunes you know or want to learn. Find your friends amongst the members and se what tunes you have in common. You can also se the sheet music for all the tunes. The music data comes from the great database The session.',
+    tags: ['react', 'node.js', 'mongoose'],
+    deployed: 'https://www.pickuplimes.com/',
+    github: 'https://github.com/HedvigM',
+  },
+  {
+    image: '',
+    name: 'Tunes & Friends',
+    discription:
+      'This is my final project on the Technigo Bootcamp. I have had this idea for a long time, and i was finally ready to build it! Sign and mark the tunes you know or want to learn. Find your friends amongst the members and se what tunes you have in common. You can also se the sheet music for all the tunes. The music data comes from the great database The session.',
+    tags: ['react', 'hej', 'hej'],
+    deployed: 'https://www.pickuplimes.com/',
+    github: 'https://github.com/HedvigM',
+  },
+  {
+    name: 'Hej 2',
+    discription:
+      'This is my final project on the Technigo Bootcamp. I have had this idea for a long time, and i was finally ready to build it! Sign and mark the tunes you know or want to learn. Find your friends amongst the members and se what tunes you have in common. You can also se the sheet music for all the tunes. The music data comes from the great database The session.',
+    tags: ['react', 'node.js', 'mongoose'],
+    deployed: 'https://www.pickuplimes.com/',
+    github: 'https://github.com/HedvigM',
+  },
+];
+
+export const OtherProjectsText = [
+  {
+    name: 'another project',
+    description: 'This is my final project on the Technigo Bootcamp.',
+    tags: ['react', 'node.js', 'mongoose'],
+    deployed: 'https://www.pickuplimes.com/',
+    github: 'https://github.com/HedvigM',
+  },
+  {
+    name: 'another project',
+    description: 'This is my final project on the Technigo Bootcamp.',
+    tags: ['react', 'node.js', 'mongoose'],
+    deployed: 'https://www.pickuplimes.com/',
+    github: 'https://github.com/HedvigM',
+  },
+];
+
+export const ThoughtsText = [
+  {
+    image: 'xxx',
+    date: 'JUL 2022',
+    name: 'Why I love to code',
+    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these. >>',
+  },
+  {
+    image: 'xxx',
+    date: 'JUL 2422',
+    name: 'Why I love to dance',
+    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these. >>',
+  },
+];
