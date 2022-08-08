78ab245ac68201c559dd46c287a52e705f06a244
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-08 18:46:02 +0200

    Create ForMore.tsx

---
+++ pages/ForMore.tsx
@@ -0,0 +1,77 @@
+import { Box, Container, Typography } from '@mui/material';
+import React from 'react';
+import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ForMoreText } from './Text';
+
+export const ForMore = () => {
+  return (
+    <Box sx={{ background: 'white' }}>
+      <Container maxWidth='md'>
+        <Box
+          sx={{ display: 'flex', justifyContent: 'center', padding: '50px' }}
+        >
+          <Typography variant='h2'>For more</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: '30px' }}>
+          {ForMoreText.map((icon, index) => (
+            <Box
+              key={index}
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                textAlign: 'center',
+                marginBottom: '50px',
+              }}
+            >
+              <Icons>
+                <Icon
+                  href={icon.href}
+                  target='_blank'
+                  rel='noreferrer noopener'
+                  aria-label={icon.ariaLabel}
+                >
+                  <FontAwesomeIcon icon={[icon.iconImage1, icon.iconImage2]} />
+                </Icon>
+              </Icons>
+              <Typography variant='h4' sx={{ color: 'black' }}>
+                {icon.icon}
+              </Typography>
+            </Box>
+          ))}
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+const Icons = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: xx-large;
+  padding: 0 10px 0px 10px;
+`;
+const Icon = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ff0063;
+  border-radius: 50%;
+  height: 60px;
+  width: 60px;
+  margin: 10px;
+  font-size: xx-large;
+  color: #ff0063;
+  cursor: pointer;
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+  :hover {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+    color: white;
+    background-color: #ff0063;
+  }
+`;
