aab170f7f9ba2b045adb4021857ffc04528f4597
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-08 18:45:55 +0200

    feature: card component

--- pages/FeaturedProjects.tsx
+++ pages/FeaturedProjects.tsx
@@ -1,4 +1,12 @@
-import { Box, Container, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Typography,
+} from '@mui/material';
 import styled from '@emotion/styled';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,7 +14,7 @@ import { projects } from './Text';
 
 export const FeaturedProjects = () => {
   return (
-    <Container sx={{ background: 'white' }}>
+    <Box sx={{ marginBottom: '50px' }}>
       <Container maxWidth='md'>
         <Box
           sx={{ display: 'flex', justifyContent: 'center', padding: '50px' }}
@@ -15,61 +23,73 @@ export const FeaturedProjects = () => {
         </Box>
         <Box
           sx={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr',
-            columnGap: '100px',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
           }}
         >
           {projects.map((project, index) => (
-            <Box key={index} sx={{ gridColumn: 'span 1' }}>
-              <ImageContainer>
-                <ImageText>{project.name}</ImageText>
-                <Image></Image>
-              </ImageContainer>
-              <Box sx={{ padding: '20px 0 70px 0' }}>
-                <Typography variant='h3'>{project.name}</Typography>
-                <Typography variant='body1'>{project.discription}</Typography>
-                <Box sx={{ marginTop: '15px' }}>
-                  {project.tags.map((tag, index) => (
-                    <Typography key={index} variant='h6'>
-                      {tag}
+            <Card
+              key={index}
+              sx={{
+                maxWidth: 400,
+                minWidth: 300,
+                margin: '10px',
+              }}
+            >
+              <CardActionArea>
+                <CardMedia></CardMedia>
+                <CardContent>
+                  <Typography variant='h3' component='div'>
+                    {project.name}
+                  </Typography>
+                  <Typography variant='body2' color='text.secondary'>
+                    {project.discription}
+                  </Typography>
+                  <Box sx={{ marginTop: '15px' }}>
+                    {project.tags.map((tag, index) => (
+                      <Typography key={index} variant='h6'>
+                        {tag}
+                      </Typography>
+                    ))}
+                    <Typography
+                      variant='h6'
+                      sx={{ backgroundColor: 'secondary.main', color: 'white' }}
+                    >
+                      <H6Variant
+                        aria-label='Link to the deployed project'
+                        aria-pressed='false'
+                        role='button'
+                        target='_blank'
+                        href={project.deployed}
+                      >
+                        Deployed project
+                      </H6Variant>
+                      <Typography
+                        variant='h6'
+                        sx={{ backgroundColor: 'secondary.main' }}
+                      >
+                        <H6Variant
+                          aria-label='Link to the deployed project'
+                          aria-pressed='false'
+                          role='button'
+                          target='_blank'
+                          href={project.github}
+                        >
+                          <FontAwesomeIcon icon={['fab', 'github']} />
+                        </H6Variant>
+                      </Typography>
                     </Typography>
-                  ))}
-                  <Typography
-                    variant='h6'
-                    sx={{ backgroundColor: 'secondary.main', color: 'white' }}
-                  >
-                    <H6Variant
-                      aria-label='Link to the deployed project'
-                      aria-pressed='false'
-                      role='button'
-                      target='_blank'
-                      href={project.deployed}
-                    >
-                      Deployed project
-                    </H6Variant>
-                  </Typography>
-                  <Typography
-                    variant='h6'
-                    sx={{ backgroundColor: 'secondary.main' }}
-                  >
-                    <H6Variant
-                      aria-label='Link to the deployed project'
-                      aria-pressed='false'
-                      role='button'
-                      target='_blank'
-                      href={project.github}
-                    >
-                      <FontAwesomeIcon icon={['fab', 'github']} />
-                    </H6Variant>
-                  </Typography>
-                </Box>
-              </Box>
-            </Box>
+                  </Box>
+                </CardContent>
+              </CardActionArea>
+            </Card>
           ))}
         </Box>
       </Container>
-    </Container>
+    </Box>
   );
 };
 
@@ -90,7 +110,7 @@ const ImageText = styled.h3`
   left: 0;
   height: 100%;
   width: 100%;
-  background-color: #86cb91;
+  /* background-color: #86cb91; */
   :hover {
     display: none;
     -webkit-transition-duration: 0.3s;
@@ -101,9 +121,12 @@ const ImageText = styled.h3`
 `;
 
 const Image = styled.div`
-  background-color: #ff0063;
-  width: 400px;
-  height: 400px;
+  /* background-color: #ff0063; */
+  background-color: pink;
+  width: 200px;
+  /*   width: 400px; */
+  height: 200px;
+  /* height: 400px; */
   display: block;
 
   :hover {
