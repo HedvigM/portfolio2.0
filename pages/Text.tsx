2eaa40ecda62b16a844f3e885e3a6ff853eaefbb
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-08 18:48:16 +0200

    feature: more text from all the components

--- pages/Text.tsx
+++ pages/Text.tsx
@@ -1,4 +1,45 @@
-import React from 'react';
+export const HeaderIconText = [
+  {
+    icon: 'GitHub',
+    iconImage1: 'fab',
+    iconImage2: 'github',
+    href: 'https://github.com/HedvigM',
+    ariaLabel: 'a link to my GitHub account',
+  },
+  {
+    icon: 'stack Overflow',
+    iconImage1: 'fab',
+    iconImage2: 'stack-overflow',
+    href: 'https://stackoverflow.com/users/16650863/hedvig',
+    ariaLabel: 'a link to my stack-overflow account',
+  },
+  {
+    icon: 'LinkedIn',
+    iconImage1: 'fab',
+    iconImage2: 'linkedin-in',
+    href: 'https://www.linkedin.com/in/hedvig-mejstedt',
+    ariaLabel: 'a link to my stack-overflow account',
+  },
+];
+
+export const HeaderText = [
+  {
+    portfolio: 'portfolio:',
+    name: 'Hedvig Mejstedt',
+    title: 'frontend developer',
+    more: '+ some thing more',
+    image: '',
+  },
+];
+
+export const PresentationText = [
+  {
+    paragraphOne:
+      'Hello! I am a frontend developer who is not afraid to venture into new situations in search of new knowledge or challenges',
+    paragraphTwo: `I am a trained social worker that recently graduated from Technigo's 24 week bootcamp where I spent every day learning about web
+    technologies. I loved every second of it, and I'm looking forward to working as a frontend developer and being a part of the community and the forward movement.`,
+  },
+];
 
 export const projects = [
   {
@@ -44,6 +85,9 @@ export const OtherProjectsText = [
     tags: ['react', 'node.js', 'mongoose'],
     deployed: 'https://www.pickuplimes.com/',
     github: 'https://github.com/HedvigM',
+    ariaLabel: 'Link to the deployed project',
+    iconImage1: 'fab',
+    iconImage2: 'github',
   },
   {
     name: 'another project',
@@ -51,6 +95,9 @@ export const OtherProjectsText = [
     tags: ['react', 'node.js', 'mongoose'],
     deployed: 'https://www.pickuplimes.com/',
     github: 'https://github.com/HedvigM',
+    ariaLabel: 'Link to the deployed project',
+    iconImage1: 'fab',
+    iconImage2: 'github',
   },
 ];
 
@@ -59,12 +106,106 @@ export const ThoughtsText = [
     image: 'xxx',
     date: 'JUL 2022',
     name: 'Why I love to code',
-    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these. >>',
+    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these.',
   },
   {
     image: 'xxx',
     date: 'JUL 2422',
     name: 'Why I love to dance',
-    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these. >>',
+    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these.',
+  },
+];
+
+export const MoreThoughtsText = [
+  {
+    date: 'Aug 2522',
+    name: 'Why I love to code',
+    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these.',
+    href: 'https://medium.com/@hedvig.mejstedt/tunes-friends-f54e995e64ef',
+  },
+  {
+    date: 'sep 2404',
+    name: 'Why I love to dance',
+    text: 'One thing that you quickly become aware of when learning to code is that there are a variety of parentheses. How to use these do not always feel as obvious, so I thought I would make a small guide on how to think about the use of these.',
+    href: 'https://medium.com/@hedvig.mejstedt/tunes-friends-f54e995e64ef',
+  },
+];
+
+export const SkillsText = [
+  {
+    name: 'code',
+    list: [
+      'JavaScript',
+      'ES6',
+      'react',
+      'Redux',
+      'NEXT.JS',
+      'Node.js',
+      '*typeScript*',
+      'CSS3',
+      'Styled Components',
+      '*Vanilla Extract*',
+      'HTML5',
+      'JSX',
+      'mongoose',
+    ],
+  },
+  {
+    name: 'toolbox',
+    list: [
+      'github',
+      'VSCode',
+      'Mongo DB',
+      'Adobe Photoshop',
+      '*Figma*',
+      'Slack',
+      'jira',
+      'Confluence',
+    ],
+  },
+  {
+    name: 'more',
+    list: [
+      'Office package',
+      '*Agile methodology*',
+      'Web Accessibility',
+      '*Flexbox*',
+      'pair-programming',
+      'Mob-programming',
+      'API:s - postman',
+    ],
+  },
+];
+
+export const ForMoreText = [
+  {
+    icon: 'GitHub',
+    iconImage1: 'fab',
+    iconImage2: 'github',
+    href: 'https://github.com/HedvigM',
+    ariaLabel: 'a link to my GitHub account',
+  },
+  {
+    icon: 'stack Overflow',
+    iconImage1: 'fab',
+    iconImage2: 'stack-overflow',
+    href: 'https://stackoverflow.com/users/16650863/hedvig',
+    ariaLabel: 'a link to my stack-overflow account',
+  },
+  {
+    icon: 'LinkedIn',
+    iconImage1: 'fab',
+    iconImage2: 'linkedin-in',
+    href: 'https://www.linkedin.com/in/hedvig-mejstedt',
+    ariaLabel: 'a link to my stack-overflow account',
+  },
+];
+
+export const ContactText = [
+  {
+    heading: 'contact',
+    name: 'Hedvig Mejstedt',
+    number: '+46 70 - 51 20 226',
+    email: 'hedvig@mejstedt.se',
   },
 ];
