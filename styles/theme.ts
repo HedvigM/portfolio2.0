d7b0b3d8d4a72e3aadacfb46be8edd4e13421728
Hedvig Mejstedt <hedvig@mejstedt.se>
2022-08-12 19:59:48 +0200

    feature: light /dark themes

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,114 +1,42 @@
 
-import { createTheme } from '@mui/material';
+import { createTheme, PaletteMode } from '@mui/material';
+import { amber, deepOrange, green, grey, lime, orange, red } from '@mui/material/colors';
 
-export const darkTheme = createTheme({
+
+export const getDesignTokens = (mode: PaletteMode) => ({
   palette: {
-    mode: 'dark',
-    primary: {
-      main: '#540b0e',
-      contrastText: '#a44200',
-    },
-   secondary: {
-     main: '#3c1518',
-     contrastText: '#f2f3ae',
-   },
-   background: {
-     default: '#03071e',
-   },
-   common: {
-     black: 'black',
-     white: 'white',
-   }
-  },
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      'Roboto',
-    ].join(','),
-    fontSize: 16,
-    h1: {
-      fontSize: '2rem',
-      fontWeight: 700,
-      fontFamily: 'Roboto',
-      textTransform: 'uppercase',
-     /*  color: '#66BFBF', */
-    },
-    h2: {
-      fontSize: '1.5rem',
-      fontWeight: 700,
-      textTransform: 'uppercase',
-      color: 'white',
-      backgroundColor: '#a44200',
-      padding: '0px 10px',
-      textAlign: 'center',
-      display: 'inline',
-      fontFamily: 'Roboto',
-    },
-    h3: {
-      fontSize: '1.2rem',
-      fontWeight: 700,
-      fontFamily: 'Roboto',
-      textTransform: 'uppercase',
-      
-    },
-    h4: {
-      fontSize: '1.15rem',
-      fontWeight: 400,
-      fontFamily: 'Roboto',
-      textTransform: 'uppercase',
-      color: '#66BFBF',
-    },
-    h5: {
-      fontSize: '1.07rem',
-      fontWeight: 700,
-      lineHeight: '1.4',
-
-    },
-    h6: {
-      fontSize: '1rem',
-      fontWeight: 300,
-      color: 'black',
-      backgroundColor: '#66BFBF',
-      padding: '0px 10px',
-      lineHeight: '1.4',
-      textAlign: 'center',
-      display: 'inline',
-      fontFamily: 'Roboto',
-      margin: '2px',
-      borderRadius: '2px',
-    },
-    body1: {
-      fontSize: '1rem',
-      fontFamily: 'Roboto',
-      textAlign: 'left',
-      lineHeight: '1.7',
-      textDecoration: 'none',
-    },
-    body2: {
-      fontSize: '1rem'
-    },
-    button: {
-      textTransform: 'none'
-    },
-  }
-});
-
-export const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#66BFBF'
-    },
-   secondary: {
-     main: '#FF0063'
-   },
-   background: {
-    default: '#EAF6F6'
-  },
-  common: {
-    black: 'black',
-    white: 'white',
-  }
+    mode,
+    ...(mode === 'light'
+      ? {
+          // palette values for light mode
+          primary: {
+            main: '#EAF6F6',
+            contrastText: '#66BFBF'
+          },
+          secondary: {
+            main: '#fff',
+            contrastText: '#FF0063',
+          },
+          text: {
+            primary: 'black',
+           secondary: 'white', 
+          },
+        }
+      : {
+          // palette values for dark mode
+           primary: {
+            main: '#121212',
+            contrastText: '#D65A31',
+          },
+          secondary: {
+            main: '#181818',
+            contrastText: '#EEEEEE',
+          },
+          text: {
+            primary: 'white',
+           secondary: 'black', 
+          },
+        }),
   },
   typography: {
     fontFamily: [
@@ -180,5 +108,5 @@ export const theme = createTheme({
     button: {
       textTransform: 'none'
     },
-  }
+  },
 });
