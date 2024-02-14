8a8fb23e3bd24d0c5eb11bc8ef5e8f1f979b184c
Hedvig Mejstedt <hedvig@mejstedt.se>
2024-02-14 21:15:25 +0100

    made svg images

--- Components/Presentation.tsx
+++ Components/Presentation.tsx
@@ -1,16 +1,19 @@
-import { Box, Container, styled } from '@mui/material';
-import React from 'react';
-import { Body1 } from 'styles/theme';
-import { PresentationText } from './Text';
+import { Box, Container, styled } from "@mui/material";
+import React from "react";
+import { Body1 } from "styles/theme";
+import { PresentationText } from "./Text";
 
 export const Presentation = () => {
   return (
-    <Box sx={{ backgroundColor: 'primary.main' }}>
-      <Container maxWidth='sm' sx={{ padding: '50px' }}>
+    <Box sx={{ backgroundColor: "primary.main" }}>
+      <Container maxWidth="sm" sx={{ padding: "50px" }}>
+        <img src="/./Pictures/zigzag.svg"></img>
+        <img src="/./Pictures/dotts.svg"></img>
+        <img src="/./Pictures/small-dotts.svg"></img>
         {PresentationText.map((text, index) => (
           <Body1 key={index}>
-            <Line aria-hidden='true'></Line>
-            <Dot aria-hidden='true'></Dot>
+            <Line aria-hidden="true"></Line>
+            <Dot aria-hidden="true"></Dot>
 
             {text.paragraphOne}
             <br />
@@ -23,20 +26,20 @@ export const Presentation = () => {
   );
 };
 
-const Line = styled('span')((props) => ({
-  border: '1px solid',
+const Line = styled("span")((props) => ({
+  border: "1px solid",
   color: props.theme.palette.secondary.contrastText,
-  width: '40px',
-  display: 'inline-block',
-  marginBottom: '4px',
+  width: "40px",
+  display: "inline-block",
+  marginBottom: "4px",
 }));
 
-const Dot = styled('span')((props) => ({
-  height: '10px',
-  width: '10px',
+const Dot = styled("span")((props) => ({
+  height: "10px",
+  width: "10px",
   backgroundColor: props.theme.palette.secondary.contrastText,
-  borderRadius: '50%',
-  display: 'inline-block',
-  position: 'relative',
-  left: '-5px',
+  borderRadius: "50%",
+  display: "inline-block",
+  position: "relative",
+  left: "-5px",
 }));
