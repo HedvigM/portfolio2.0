fdac3af3ec087b7d6f745b843ae33f41efa630a7
Hedvig Mejstedt <hedvig@mejstedt.se>
2024-02-16 09:26:42 +0100

    Header remake

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,138 +1,95 @@
-import { Avatar, Box, Container, styled } from '@mui/material';
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { HeaderText, IconText } from './Text';
-import { H1 } from 'styles/theme';
+import { Avatar, Box, Container, Typography, styled } from "@mui/material";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { HeaderText, IconText } from "./Text";
+import { H1 } from "styles/theme";
 
 export const Header = () => {
   library.add(fab);
   return (
-    <BGImage>
-      <Container maxWidth='md'>
-        <Box sx={{ display: 'flex' }}>
-          <Icons>
-            {IconText.map((text, index) => (
-              <Icon
-                key={index}
-                href={text.href}
-                target='_blank'
-                rel='noreferrer noopener'
-                aria-label={text.ariaLabel}
-              >
-                <FontAwesomeIcon icon={text.icon} />
-              </Icon>
-            ))}
-          </Icons>
-        </Box>
-        {HeaderText.map((text, index) => (
-          <Box
+    <MainContainer>
+      {/*  <Icons>
+        {IconText.map((text, index) => (
+          <Icon
             key={index}
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-around',
-            }}
+            href={text.href}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label={text.ariaLabel}
           >
-            <Box
-              sx={{
-                maxWidth: '200px',
-              }}
-            >
-              <Box
-                sx={{
-                  color: 'white',
-                  textTransform: 'uppercase',
-                  fontSize: 'large',
-                }}
-              >
-                <p>
-                  {text.portfolio}
-                  <br />
-                  <strong>{text.name}</strong>
-                </p>
-              </Box>
-              <Box>
-                <H1>{text.title}</H1>
-              </Box>
-              <Box
-                sx={{
-                  color: 'white',
-                  textTransform: 'uppercase',
-                  fontSize: 'large',
-                }}
-              >
-                <p>{text.more}</p>
-              </Box>
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'end',
-              }}
-            >
-              <Avatar
-                sx={{
-                  width: '150px',
-                  height: '150px',
-                  bottom: '-120px',
-                  border: '3px solid white',
-                }}
-              >
-                <ProfilePic
-                  src='/./Pictures/profile-pic.jpg'
-                  alt='profile picture'
-                />
-              </Avatar>
-            </Box>
-          </Box>
+            <FontAwesomeIcon icon={text.icon} />
+          </Icon>
         ))}
-      </Container>
-    </BGImage>
+      </Icons> */}
+
+      {/* <H1>Hej och välkomna!</H1> */}
+      <Div>
+        <Typography variant="body1" textAlign="left">
+          👋 Hello! I'm Hedvig
+        </Typography>
+      </Div>
+      <Typography variant="h6">
+        I'm a front-end developer, som bara skall komma på lite go text att
+        skriva här
+      </Typography>
+
+      {/*  <img
+        src="/./Pictures/small-dotts.svg"
+        style={{
+          position: "relative",
+          top: "0",
+          left: "0",
+          height: "100px",
+        }}
+      ></img>
+      <ProfilePic src="/./Pictures/profile-pic.jpg" alt="profile picture" /> */}
+    </MainContainer>
   );
 };
 
-const ProfilePic = styled('img')((props) => ({
-  height: '100%',
-}));
+const ProfilePic = styled("img")({
+  height: "100px",
+});
 
-const BGImage = styled('div')((props) => ({
-  background:
-    'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./Pictures/mountains.JPG)',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  position: 'relative',
-}));
+const MainContainer = styled("div")({
+  height: `calc(100vh - 50px)`,
+  padding: "20px",
+  display: "grid",
+  gridTemplateRows: "1fr 2fr",
+});
+const Div = styled("div")({
+  transform: `rotate(-4deg)`,
+  padding: "10px",
+  placeSelf: "start",
+  alignSelf: "end",
+});
 
-const Icons = styled('div')((props) => ({
-  display: 'flex',
-  justifyContent: 'end',
-  width: '100%',
-  fontSize: 'xx-large',
-  padding: '10px',
-}));
+const Icons = styled("div")({
+  display: "flex",
+  justifyContent: "end",
+  width: "100%",
+  fontSize: "xx-large",
+  padding: "10px",
+});
 
-const Icon = styled('a')((props) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  border: '1px solid',
-  borderRadius: '50%',
-  height: '60px',
-  width: '60px',
-  margin: '10px',
-  fontSize: 'xx-large',
+const Icon = styled("a")((props) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "1px solid",
+  borderRadius: "50%",
+  height: "60px",
+  width: "60px",
+  margin: "10px",
+  fontSize: "xx-large",
   color: props.theme.palette.secondary.contrastText,
-  cursor: 'pointer',
-  /*  -webkit-transition-duration: 0.3s,
-  transition-duration: 0.3s;
-  -webkit-transition-property: transform;
-  transition-property: transform; */
-  '&:hover': {
-    /*  -webkitTransform: 'scale(1.5)',  */
-    transform: 'scale(1.1)',
+  cursor: "pointer",
+  "&:hover": {
+    transform: "scale(1.1)",
     color: props.theme.palette.text.secondary,
     backgroundColor: props.theme.palette.secondary.contrastText,
-    border: 'none',
+    border: "none",
   },
 }));
