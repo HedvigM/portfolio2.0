feacdc94279f46d513f31fe678bb3f295189ad71
Hedvig Mejstedt <hedvig@mejstedt.se>
2024-02-16 09:26:58 +0100

    FeaturedProjects remake

--- Components/FeaturedProjects.tsx
+++ Components/FeaturedProjects.tsx
@@ -1,152 +1,76 @@
-import {
-  Box,
-  Card,
-  CardContent,
-  CardMedia,
-  Container,
-  styled,
-} from '@mui/material';
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { projects } from './Text';
-import { Body1, H2, H3, H6, H6Link } from 'styles/theme';
+import { Typography, styled } from "@mui/material";
+import React from "react";
+import { projects } from "./Text";
+
+const MainContainer = styled("div")((props) => ({
+  height: `calc(100vh)`,
+  padding: "30px 30px 50px 30px",
+  display: "grid",
+  gridTemplateRows: "110px 1fr",
+  backgroundColor: props.theme.palette.primary.main,
+}));
+const ProjectsContainer = styled("div")((props) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "30px",
+}));
+const ProjectContainer = styled("div")((props) => ({
+  boxShadow: `10px 5px ${props.theme.palette.tritiary.main}`,
+  backgroundImage: `url(${props.image})`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  border: `1px solid ${props.theme.palette.tritiary.main}`,
+  borderRadius: "5px",
+  width: "100%",
+  height: "100%",
+  position: "relative",
+
+  ":hover": {
+    cursor: "pointer",
+  },
+  ":active": {
+    boxShadow: "none",
+  },
+}));
+const Title = styled("div")((props) => ({
+  border: `1px solid ${props.theme.palette.tritiary.main}`,
+  borderRadius: "5px",
+  padding: "2px",
+  width: "225px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  boxShadow: `10px 5px ${props.theme.palette.tritiary.main}`,
+  backgroundColor: "white",
+  position: "absolute",
+  top: "-10px",
+  left: "30px",
+}));
 
 export const FeaturedProjects = () => {
   return (
-    <Box sx={{ backgroundColor: 'secondary.main', paddingBottom: '50px' }}>
-      <Container maxWidth='md'>
-        <Box
-          sx={{ display: 'flex', justifyContent: 'center', padding: '50px' }}
-        >
-          <H2>Featured Projects</H2>
-        </Box>
-        <Box
-          sx={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            flexWrap: 'wrap',
-          }}
-        >
-          {projects.map((project, index) => (
-            <Card
-              key={index}
-              sx={{
-                maxWidth: 400,
-                minWidth: 300,
-                margin: '10px',
-              }}
-            >
-              <Box
-                sx={{
-                  position: 'relative',
-                  overflow: 'hidden',
-                }}
-              >
-                <Overlay>
-                  <P>{project.name}</P>
-                </Overlay>
-                <CardMedia
-                  sx={[
-                    {
-                      /*  filter: 'brightness(70%)', */
-
-                      '&:hover': {
-                        transition: '0.3s',
-                        transform: 'scale(1.1)',
-                        filter: 'brightness(100%)',
-                      },
-                    },
-                  ]}
-                  component='img'
-                  height='250'
-                  image={project.image}
-                  alt={project.alt}
-                ></CardMedia>
-              </Box>
-              <CardContent>
-                <H3>{project.name}</H3>
-                <Body1>{project.description}</Body1>
-                <Box
-                  sx={{
-                    marginTop: '15px',
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                  }}
-                >
-                  {project.tags.map((tag, index) => (
-                    <Box key={index}>
-                      <H6>{tag}</H6>
-                    </Box>
-                  ))}
-                  <Box>
-                    <H6Link>
-                      <a
-                        aria-label='Link to the deployed project'
-                        aria-pressed='false'
-                        role='button'
-                        target='_blank'
-                        href={project.deployed}
-                        style={{ textDecoration: 'none', color: 'inherit' }}
-                      >
-                        Deployed project
-                      </a>
-                    </H6Link>
-                  </Box>
-                  <Box>
-                    <H6Link>
-                      <a
-                        aria-label='Link to the deployed project'
-                        aria-pressed='false'
-                        role='button'
-                        target='_blank'
-                        href={project.github}
-                        style={{ textDecoration: 'none', color: 'inherit' }}
-                      >
-                        <FontAwesomeIcon icon={['fab', 'github']} />
-                      </a>
-                    </H6Link>
-                  </Box>
-                </Box>
-              </CardContent>
-            </Card>
-          ))}
-        </Box>
-      </Container>
-    </Box>
+    <MainContainer>
+      <Typography variant="h2" textAlign="left">
+        Projects
+      </Typography>
+      <ProjectsContainer>
+        {projects.map((project, index) => (
+          <ProjectContainer
+            image={project.image}
+            onClick={() => {
+              window.open(project.deployed, "_blank", "noopener");
+            }}
+          >
+            <Title>
+              <Typography key={index} variant="h6" textAlign="left">
+                {project.name}
+              </Typography>
+            </Title>
+          </ProjectContainer>
+        ))}
+      </ProjectsContainer>
+    </MainContainer>
   );
 };
-
-const Overlay = styled('div')((props) => ({
-  position: 'absolute',
-  display: 'inline',
-  top: '0px',
-  right: '0px',
-  bottom: '0px',
-  left: '0px',
-  pointerEvents: 'none',
-  /* background: rgb(1, 1, 1); */
-  background: '#2c2c2c7b',
-  transition: '0.1s ease-in',
-}));
-
-const P = styled('p')((props) => ({
-  position: 'absolute',
-  fontWeight: '200',
-  color: 'white',
-  display: 'flex',
-  fontFamily: 'Roboto',
-  fontSize: '25px',
-  top: '0',
-  bottom: '0',
-  right: '0',
-  left: '0',
-  margin: '0',
-  justifyContent: 'center',
-  alignItems: 'center',
-
-  '&:hover': {
-    color: 'transparent',
-  },
-}));
